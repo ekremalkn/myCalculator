94e0356b6b48bead4e9837e80f9c97f1b329602c
Ekrem Alkan <ae.alkanekrem@gmail.com>
2022-09-08 18:31:59 +0300

    Initial Commit

---
+++ calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  calculator
+//
+//  Created by Ekrem Alkan on 8.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
